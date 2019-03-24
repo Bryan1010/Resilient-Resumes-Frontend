d5bbaecbd1ac78748d7eb3f3191f14735ede3dda
Bryan Cruz <Bryan@innerbounce.com>
2019-03-24 12:50:16 -0400

    added sample submition funciton to JS

--- js/app.js
+++ js/app.js
@@ -186,6 +186,33 @@ function addNewActivity() {
     container.appendChild(div);
 }
 
+function sampleSumbmition() {
+    let data = JSON.stringify(sampleData);
+    // console.log(data);
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": url,
+        "method": "POST",
+        "headers": {
+            "Content-Type": "application/json",
+            "cache-control": "no-cache",
+            "Postman-Token": "51243b11-97b2-4c2a-a8b1-d649dff221a4"
+        },
+        "processData": false,
+        "data": data
+    }
+
+    $.ajax(settings).done(function (response) {
+        // localStorage.setItem('Resume', JSON.stringify(response));
+        console.log(response);
+        $('#content').html(response);
+
+        $('.feedback-section').tooltip()
+        // alert('done');
+    });
+}
+
 var sampleData = {
     "Name": {
         "FName": "Bryan",
