c0a0b57246fb728cef141e833ce0dd723177eeeb
Bryan Cruz <Bryan@innerbounce.com>
2019-05-05 22:12:04 -0400

    comiting semiworking express login process with pug templating

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const userController = require('../controllers/userController');
+const authController = require('../controllers/authController');
 
-router.get('/', (req, res) => {
-    res.send('IT WORKS!!');
+router.get('/', (req,res)=> {
+    res.render('home');
 });
+
+router.get('/login', userController.loginForm);
+
+router.get('/register', userController.registerForm);
+
+// 1. Validate the registration data
+// 2. register the user
+// 3. we need to log them in
+router.post('/register',
+  userController.validateRegister,
+  userController.register,
+  authController.login
+);
 
 module.exports = router;
--- start.js
+++ start.js
@@ -1,29 +1,22 @@
-const mongoose = require("mongoose");
-const consola = require('consola');
+const mongoose = require('mongoose');
+var mongoClient = require("mongodb").MongoClient;
 
-// import env variables from our variables.env file
+// import environmental variables from our variables.env file
 require('dotenv').config({ path: 'variables.env' });
 
-// console.log(`💥💥 ${process.env.DATABASE}`);
-// Connect to DB and handle bad connections
-// mongoClient.connect("mongodb://dev-resilient-resumes:VlizGl78drmrrDW66C4Qv6vx6tnUyEiVLzy1Hhu7PxcNtRgLMg0W4NiU5HkbwgGknnpF6VQgrkIwN9euvGkz8A%3D%3D@dev-resilient-resumes.documents.azure.com:10255/ResilientResumes?ssl=true", { useNewUrlParser: true }, function (err, client) {
-//     console.error(err.message);
-//     client.close();
-// });
+// Connect to our Database and handle any bad connections
 mongoose.connect(process.env.DATABASE, { useNewUrlParser: true });
 mongoose.Promise = global.Promise;
 mongoose.connection.on('error', (err) => {
-    console.error(`DB Connection Error🙅 🚫 🙅 🚫 🙅 🚫 🙅 🚫 → ${err.message}`);
+    console.error(`DB Connection Error 🙅 🚫 🙅 🚫 🙅 🚫 🙅 🚫 → ${err.message}`);
 });
 
-// Start app
+//import data modelsˀ
+require(`./models/users`);
+
+// Start our app!
 const app = require('./app');
 app.set('port', process.env.PORT || 7777);
 const server = app.listen(app.get('port'), () => {
-    consola.ready({
-        message: `Server listening on 
-        http://${server.address().host}:${server.address().port}`,
-        badge: true
-    })
-    console.log(`Express running ➡️ PORT ${server.address().port}`)
+  console.log(`💻 Express running → PORT ${server.address().port}`);
 });
