d919271af5d8ccb9b2f68b8b2f024b8b88fb6b60
Amie Croteau <ac04400@ravens.racc.edu>
2019-05-07 17:26:38 -0400

    added form.vue not connected to template yet

--- app/src/main.js
+++ app/src/main.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import './plugins/vuetify'
 import Template from './Template.vue'
+import('../node_modules/vuetify/dist/vuetify.min.css')
+import '../node_modules/vuetify/src/stylus/main.styl'
+
+
+
 
 Vue.config.productionTip = false
 
