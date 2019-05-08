647e5a997a004b5f94368cbca1d43090bb87992a
Amie Croteau <ac04400@ravens.racc.edu>
2019-05-07 20:26:51 -0400

    Login and second button created with PSU colors #093162

--- app/src/Template.vue
+++ app/src/Template.vue
@@ -1,30 +1,19 @@
 <template>
-  <v-app id="inspire">
+  <v-app>
     <v-content>
       <v-container fluid fill-height>
+      
+
+
         <v-layout align-center justify-center>
           <v-flex xs12 sm8 md4>
             <v-card class="elevation-12">
-              <v-toolbar dark color="primary">
-                <v-toolbar-title>Login form</v-toolbar-title>
+              <v-toolbar dark color="#093162">
+                <v-toolbar-title>Login</v-toolbar-title>
                 <v-spacer></v-spacer>
-                <v-tooltip bottom>
-                  <template v-slot:activator="{ on }">
-                    <v-btn :href="source" icon large target="_blank" v-on="on">
-                      <v-icon large>code</v-icon>
-                    </v-btn>
-                  </template>
-                  <span>Source</span>
-                </v-tooltip>
-                <v-tooltip right>
-                  <template v-slot:activator="{ on }">
-                    <v-btn icon large href="https://codepen.io/johnjleider/pen/wyYVVj" target="_blank" v-on="on">
-                      <v-icon large>mdi-codepen</v-icon>
-                    </v-btn>
-                  </template>
-                  <span>Codepen</span>
-                </v-tooltip>
+                           
               </v-toolbar>
+
               <v-card-text>
                 <v-form>
                   <v-text-field prepend-icon="person" name="login" label="Login" type="text"></v-text-field>
@@ -33,7 +22,10 @@
               </v-card-text>
               <v-card-actions>
                 <v-spacer></v-spacer>
-                <v-btn color="primary">Login</v-btn>
+                <v-btn dark color="#093162">Login</v-btn>
+               
+                <v-btn dark color="#093162">Create Profile</v-btn>
+                <v-spacer></v-spacer>
               </v-card-actions>
             </v-card>
           </v-flex>
--- app/src/main.js
+++ app/src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import './plugins/vuetify'
 import Template from './Template.vue'
-import('../node_modules/vuetify/dist/vuetify.min.css')
+import'../node_modules/vuetify/dist/vuetify.min.css'
 import '../node_modules/vuetify/src/stylus/main.styl'
 
 
