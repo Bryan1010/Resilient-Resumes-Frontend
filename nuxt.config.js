3281eaf1d5a8960fc1dcba0169e5246e10e73747
Bryan Cruz <Bryan@innerbounce.com>
2019-06-30 01:34:52 -0400

    adding axios plugin to intercept requests

--- nuxt.config.js
+++ nuxt.config.js
@@ -37,7 +37,7 @@ module.exports = {
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: [],
+  plugins: ['~/plugins/axios'],
   /*
    ** Nuxt.js modules
    */
---
+++ plugins/axios.js
@@ -0,0 +1,34 @@
+import Cookies from 'js-cookie'
+
+export default ({ app: { $axios }, res, req, store, redirect }) => {
+  $axios.setHeader('Content-Type', 'application/json')
+  $axios.setHeader('X-Requested-With', 'XMLHttpRequest')
+  $axios.defaults.baseURL = 'http://homestead1.test'
+
+  $axios.interceptors.response.use(
+    function(response) {
+      if (response.headers.authorization) {
+        const token = response.headers.authorization.split('Bearer ')
+        if (process.server) {
+          res.cookie('mg_t', token[1]) // ssr only
+        } else {
+          Cookies.set('mg_t', token[1])
+        }
+        store.commit('login/UPDATE_AUTH_COOKIE', token[1])
+      }
+      return response
+    },
+    function(error) {
+      if (error.response.headers.authorization) {
+        const token = error.response.headers.authorization.split('Bearer ')
+        if (process.server) {
+          res.cookie('mg_t', token[1]) // ssr only
+        } else {
+          Cookies.set('mg_t', token[1])
+        }
+        store.commit('login/UPDATE_AUTH_COOKIE', token[1])
+      }
+      return Promise.reject(error)
+    }
+  )
+}
