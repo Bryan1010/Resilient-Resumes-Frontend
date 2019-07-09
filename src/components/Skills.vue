6da9e87c5bcaba0efae7b99b5a9a5843403b9d4e
Amie Croteau <ac04400@ravens.racc.edu>
2019-07-09 16:58:19 -0400

    Updated skills component

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -14,7 +14,7 @@
             </v-card-text>
           </v-card>
           <v-card>
-            <v-card-text class="primary--text"><span>The skills don't have to be the greatest ever, be honest and don't focus on latest technologies unless you are familiar with them</span></v-card-text>
+            <v-card-text class="primary--text"><span>The skills don't have to be the best buzzwords, be honest and do not focus on latest technologies unless you are familiar with them</span></v-card-text>
           </v-card>
           <v-card>
             <v-card-text class="primary--text"><span>Be sure to list your level of familiarity. </span></v-card-text>
@@ -29,33 +29,35 @@
   
     
           <v-form  ref="form" id="form" class="px-5">
-          
-       <v-tooltip right>
-        <template v-slot:activator="{ on }">
         <v-text-field
-              label="What is your objective statement?"
-              name="objective"
-              placeholder="Highly-motivated Information Science and Technology undergraduate with a 3.9 GPA looking to fill a position as a Database Intern."
-              v-model="objective"
-              prepend-icon="work"
-              :rules="inputRules"
-              v-on="on"
+              label="How many skills would you like to list?"
+              name="numSkills"
+              v-model="numSkills"
+              type="number"
+             
+              
             ></v-text-field>
-            </template>
-        <span>It is highly recommended to include an objective statement.</span>
-      </v-tooltip>
             
         <v-text-field
         
-              label="What position are you applying for?"
-              name="position"
-              placeholder="IST Summer Internship"
-              v-model="job"
-              prepend-icon="work"
-              :rules="inputRules"
+              label="What is the name of the skill?"
+              name="skill"
+         
+              v-model="skill"
+                       
             ></v-text-field>
-           
-   
+ <v-tooltip top>
+            <template v-slot:activator="{ on }">  
+        <v-combobox
+        
+            v-model="selectlevel"
+            :items="items"
+            name="skillLevel"
+            label="What is your level of knowledge for this skill?"
+          ></v-combobox>
+          </template>
+ <span>Select beginner if you just learned the skill in a class, intermediate if you have done a project in it and advanced if you are considered a resource in this area. </span>
+      </v-tooltip>
           
 </v-form>
         </v-container>
@@ -68,8 +70,16 @@ export default {
     components:{  },
   data() {
     return {
-        objective:"",
-      job:""
+        numSkills:"",
+      skill:"",
+      Actdesc:"",
+      items: [
+        'Learning',
+        'Beginner',
+        'Intermediate',
+        'Advanced'
+      ],
+
      };
   },
   methods: {
