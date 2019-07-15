8b9a80933531e8266e221e636a9d3c992e6d2417
Bryan Cruz <Bryan@innerbounce.com>
2019-07-15 19:46:55 -0400

    Added Address model

---
+++ server/models/Address.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const addressSchema = new Schema({
+  User: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+    required: 'You must supply an owner!'
+  },
+  PrimaryAddress: {
+    type: Boolean,
+    default: false
+  },
+  Line1: {
+    type: String,
+    trim: true,
+    required: 'Address Line 1 is empty, please suply a value for this field'
+  },
+  Line2: {
+    type: String,
+    trim: true
+  },
+  City: {
+    type: String,
+    trim: true
+  },
+  State: {
+    type: String,
+    trim: true
+  },
+  Zip: {
+    type: String,
+    trim: true
+  },
+  Country: {
+    type: String,
+    trim: true
+  }
+})
+
+module.exports = mongoose.model('Address', addressSchema)
