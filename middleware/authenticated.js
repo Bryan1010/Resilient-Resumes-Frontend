0fcb0530903efd7f16173789af6b010e4c802760
Bryan Cruz <Bryan@innerbounce.com>
2019-07-21 11:46:33 -0400

    fixed boolean logic on authenticated middleware

--- middleware/authenticated.js
+++ middleware/authenticated.js
@@ -1,6 +1,9 @@
+const Cookie = require('js-cookie')
+
 export default function ({ store, redirect }) {
   // If the user is not authenticated
-  if (!store.state.auth) {
+  if (store.state.login.auth === null || store.state.login.auth === '') {
+    Cookie.get('auth')
     return redirect('/login')
   }
 }
