b44766abf1c9cdb457c24e9953e5cadb84bc02c1
Bryan Cruz <Bryan@innerbounce.com>
2019-07-25 23:04:25 -0400

    started creating resume card component

---
+++ components/resume/ResumeCards.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-card>
+    <v-card-title>
+      Resume Title
+    </v-card-title>
+    <v-card-actions>
+      <v-btn>
+        <v-icon left>
+          edit
+        </v-icon>
+        Edit
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      resume: {}
+    }
+  }
+}
+</script>
