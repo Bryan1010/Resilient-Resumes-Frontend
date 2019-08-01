550a89b99cbbd4bb96a989e81c35443718126a2c
Bryan Cruz <Bryan@innerbounce.com>
2019-08-01 18:54:53 -0400

    started to do save resume route

--- server/routes/resume.js
+++ server/routes/resume.js
@@ -76,4 +76,54 @@ router.post('/create', async (req, res) => {
   }
 })
 
+router.post('/save', (req, res) => {
+  let resumeID = ''
+  if (req.body.resumeID) resumeID = req.body.resumeID
+
+  // User Token decrypt
+  let _userId = ''
+  if (req.body._Userid) { _userId = req.body._Userid }
+  // if (req.query._Userid) { _userId = req.query._id }
+  if (_userId === '') {
+    return res.status(400).send(
+      { status: process.env.API_STATUS_FAILED,
+        message: 'Invalid Request, no User ID found' })
+  }
+  const token = jwt.verify(_userId, process.env.JWT_TOKEN_PRIVATE_KEY)
+
+  // if no resumeid is passed, it's a treat as new resume
+  if (resumeID === '') {
+    const resume = null
+
+    const currentUser = new User().findById(token._id,
+      {
+        Phone: 1,
+        Email: 1,
+        _id: 0
+      }
+    )
+
+    const resumeObj = {
+      User: token._id,
+      Name: token.Name,
+      ContactInfo: {
+        Email: currentUser.Email,
+        Phone: currentUser.Phone
+      },
+      ObjectiveStatement: req.body.ObjectiveStatement,
+      PositionApplyingFor: req.body.PositionApplyingFor,
+      Achievements: req.body.Achievements,
+      Activities: req.body.Activities,
+      Experience: req.body.Experience,
+      RelevantCourse: req.body.RelevantCourse,
+      School: req.body.School,
+      Skill: req.body.Skill,
+      Websites: req.body.Websites
+
+    }
+  } else {
+    const resume = new Resume().findById(resumeID)
+  }
+})
+
 module.exports = router
