605c9503bc8268dff723f2c980e07198a73abf9f
Bryan Cruz <Bryan@innerbounce.com>
2019-08-26 21:30:52 -0400

    removed commented code on default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -12,23 +12,6 @@
         </v-list-tile>
       </v-list>
     </v-navigation-drawer>
-    <!-- <v-toolbar :clipped-left="clipped" fixed app>
-      <v-toolbar-side-icon @click="drawer = !drawer" />
-      <v-btn icon @click.stop="miniVariant = !miniVariant">
-        <v-icon>{{ `chevron_${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="clipped = !clipped">
-        <v-icon>web</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="fixed = !fixed">
-        <v-icon>remove</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title" />
-      <v-spacer />
-      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
-        <v-icon>menu</v-icon>
-      </v-btn>
-    </v-toolbar> -->
     <v-content>
       <v-container>
         <nuxt />
